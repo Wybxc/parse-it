1426bd6a52d80fbd6010d176d184ed7d6927cf73
忘忧北萱草 <wybxc@qq.com>
2025-02-18 22:50:34 +0800

    chore: add doc for parse-it crate

--- parse-it/src/lib.rs
+++ parse-it/src/lib.rs
@@ -1,5 +1,52 @@
+//! # Parse It
+//! 
+//! *A user-friendly, opinionated parser generator for Rust.*
+//! 
+//! ## Example
+//! 
+//! ```rust
+//! use parse_it::{ParseIt, parse_it};
+//! 
+//! #[derive(Debug, Clone)]
+//! pub enum Instr {
+//!     Left,
+//!     Right,
+//!     Incr,
+//!     Decr,
+//!     Read,
+//!     Write,
+//!     Loop(Vec<Self>),
+//! }
+//! 
+//! parse_it! {
+//!     mod parse {
+//!         use super::Instr;
+//! 
+//!         pub Brainfuck -> Vec<Instr> {
+//!             Primitive* => self,
+//!         }
+//! 
+//!         Primitive -> Instr {
+//!             '<' => Instr::Left,
+//!             '>' => Instr::Right,
+//!             '+' => Instr::Incr,
+//!             '-' => Instr::Decr,
+//!             ',' => Instr::Read,
+//!             '.' => Instr::Write,
+//!             '[' Primitive+ ']' => Instr::Loop(self)
+//!         }
+//!     }
+//! }
+//! 
+//! fn main() {
+//!     let parser = parse::Brainfuck::default();
+//!     let src = "--[>--->->->++>-<<<<<-------]>--.>---------.>--..+++.>----.>+++++++++.<<.+++.------.<-.>>+";
+//!     let instrs = parser.parse(src).unwrap();
+//!     println!("{:?}", instrs);
+//! }
+//! ```
 #![warn(missing_docs)]
-#![doc=include_str!("../../README.md")]
+#![allow(clippy::needless_doctest_main)]
 
 pub mod lexer;
 pub mod memo;
